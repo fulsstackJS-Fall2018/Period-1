e5424b4425ee4b36a1c8f5059757f00bf32b8669
Lars-m <lam@cphbusiness.dk>
2019-02-02 18:33:13 +0100

    Used for demo in the promise lesson

---
+++ code/myOwnPromise.js
@@ -0,0 +1,11 @@
+function myPromise(msg,delay){
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      let error = false; // Math.random() * 10 < 2;
+      if(error){
+        return reject(new Error("UPPPs"));
+      }
+      return resolve(msg.toUpperCase());
+    },delay);
+  })
+}
